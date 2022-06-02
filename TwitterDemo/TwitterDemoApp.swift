a77de8a1709009454d7e3a5af9689ac3aeac3fc1
Artem Paliutin <pal.artem@icloud.com>
2022-06-02 18:33:41 +0300

    Initial Commit

---
+++ TwitterDemo/TwitterDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterDemoApp.swift
+//  TwitterDemo
+//
+//  Created by Artem Paliutin on 02/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TwitterDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
